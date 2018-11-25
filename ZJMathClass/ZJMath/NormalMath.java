fc5415588e24803988fcd3dfca06eee817d924b5
Alexander Schmidt <main.schmidt.alexander@gmail.com>
2018-11-25 23:57:34 +0100

    TTT und MathClass

---
+++ ZJMathClass/ZJMath/NormalMath.java
@@ -0,0 +1,31 @@
+package ZJMath;
+
+public class NormalMath
+{
+	public double add(double value1, double value2)
+	{
+		return value1 + value2;		
+	}
+	
+	public double sub(double value1, double value2)
+	{
+		return value1 - value2;		
+	}
+	
+	public double mul(double value1, double value2)
+	{
+		return value1 * value2;		
+	}
+	
+	public double div(double value1, double value2)
+	{
+		return value1 / value2;		
+	}
+	
+	public double mod(double value1, double value2)
+	{
+		return value1 % value2;		
+	}
+	
+	
+}
---
+++ ZJMathClass/a.java
@@ -0,0 +1,13 @@
+import ZJMath.NormalMath;
+
+public class a
+{	
+	public static void main(String[] args)
+	{
+		NormalMath calc = new NormalMath();
+		int g = 2;
+		
+		System.out.print(calc.mul(g,2));
+	}
+
+}
