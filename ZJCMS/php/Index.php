070bee3caea2e76cc53371e5740893eb72d56d3a
Alexander Schmidt <main.schmidt.alexander@gmail.com>
2019-03-04 00:07:24 +0100

    dfh

---
+++ ZJCMS/php/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/mainstyle.css">
+<title></title>
+</head>
+<body>
+<div class="OuterMainContainer">
+	<div class="OuterTempContainer">	
+			<div class="StartingpageContentContainer">
+				<div class="SCC_Left">
+					<a href="Public.php"><span class="SCC_left_span">Public</span></a>
+				</div>
+				<div class="SCC_Right">
+					<a href="Private.php"><span class="SCC_right_span">Private</span></a>
+				</div>
+			</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ ZJCMS/php/Private.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/mainstyle.css">
+<title></title>
+</head>
+<body>
+<div class="OuterMainContainer">
+	<div class="OuterTempContainer">	
+			<div class="PublicContentContainer">
+				asdvngvgh<br/> adasdasdd	
+				asdvngvgh<br/> adasdasdd
+			</div>
+	</div>
+</div>
+</body>
+</html>
